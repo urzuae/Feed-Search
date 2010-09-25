e82c0a567014b0362709371b78c67dd669512d93
Urzuae <urzuae@gmail.com>
2010-09-25 13:52:08 -0500

    Refactor search of entries

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -12,32 +12,35 @@ class Feed < ActiveRecord::Base
   def get_matches(name, param1, param2)
     matches_name_param2 = []
     matches_name_param1 = []
-    matches_name = single_match(name)
-    matches_param1 = single_match(param1)
-    matches_param2 = single_match(param2)
-    matches_name.each do |entry|
-      if matches_param1.include?(entry)
-        matches_name_param1 << entry
-      end
-      if matches_param2.include?(entry)
-        matches_name_param2 << entry
-      end
-    end
+    rss_entries = parsing
+    matches_name = find_single_match(name, rss_entries)
+    matches_param1 = find_single_match(param1, rss_entries)
+    matches_param2 = find_single_match(param2, rss_entries)
+    matches_name_param1 = double_match(matches_name, matches_param1)
+    matches_name_param2 = double_match(matches_name, matches_param2)
     return [matches_name_param1, matches_name_param2, matches_param1, matches_param2]
   end
   
-  def single_match(param)
+  def double_match(first_array, second_array)
+    matches_first_second = []
+    second_array.each do |entry|
+      if first_array.include?(entry)
+        matches_first_second << entry
+      end
+    end
+    matches_first_second
+  end
+  
+  def find_single_match(param, rss_entries)
     param = Regexp.new(param, true)
     matches = []
-    rss_entries = parsing
-    rss_entries = rss_entries.entries
-    rss_entries.each do |entry|
+    rss_entries.entries.each do |entry|
       unless entry.summary.nil? || entry.blank?
         if entry.summary.match(param)
           matches << entry
         end
       end
     end
-    return matches
+    matches
   end
 end
